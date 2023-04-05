77bca8328593e5393db95195037d843b5d8415cc
manu <manu_1025@outlook.com>
2023-04-04 21:35:05 -0300

    3 secciones nuevas

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-const nav = document.querySelector("#nav")
-const abrir = document.querySelector("#abrir")
-const cerrar = document.querySelector("#cerrar")
+function menuDesplegable(){
+    const nav = document.querySelector("#nav")
+    const abrir = document.querySelector("#abrir")
+    const cerrar = document.querySelector("#cerrar")
 
-abrir.addEventListener("click", () => {
-    nav.classList.add("visible")
-})
+    abrir.addEventListener("click", () => {
+        nav.classList.add("visible")
+    })
 
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible")
+    cerrar.addEventListener("click", () => {
+        nav.classList.remove("visible")
 })
+}
+
+function catalogo(){
+    const contenedorHTML = document.getElementById("catalogo")
+    fetch("../db/database.json")
+    .then(response => response.json())
+    .then(data => {
+        for(producto of data){
+            contenedorHTML.innerHTML += `
+            <div class="elemento">
+                <img src=${producto.url}/>
+                <div class="precio">
+                    <h5>${producto.tipo}</h5>
+                </div>
+                <div class="precio">
+                    <a href="#">${producto.precio}</a>
+                </div>
+                
+            </div>
+            `
+        }   
+    })
+    .catch(error => console.log(error))
+}
+
+catalogo();
+menuDesplegable();
