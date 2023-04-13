443c5302064310be93b78d3a24b4dd2aa126f0ac
manu <manu_1025@outlook.com>
2023-04-12 23:16:37 -0300

    ajustes finales

--- script.js
+++ script.js
@@ -1,61 +1,58 @@
+function formulario(){
+    const socio = document.querySelector("#agregar-socio")
+    const formulario = document.querySelector("#formulario")
+    const tarjeta = document.querySelector(".tarjeta-socio")
+    const usuarios = []
+    tarjeta.style.display = "none"
 
-const socio = document.querySelector("#agregar-socio")
-const formulario = document.querySelector("#formulario")
-const tarjeta = document.querySelector(".tarjeta-socio")
-const usuarios = []
-tarjeta.style.display = "none"
+    const tarjetaClub = () => {
+        formulario.style.display = "none"
+        for( const usuario of usuarios){
+        tarjeta.innerHTML = `
+        <div id="nueva-tarjeta">
+            <h2>${usuario.nombre} ${usuario.apellido}</h2>
 
-const tarjetaClub = () => {
-    formulario.style.display = "none"
-    for( const usuario of usuarios){
-    tarjeta.innerHTML = `
-    <div id="nueva-tarjeta">
-        <h2>${usuario.nombre} ${usuario.apellido}</h2>
+            <p>¡Bienvenid@ a nuestro club!</p>
 
-        <p>¡Bienvenid@ a nuestro club!</p>
+            <p>Te dejamos un regalito de bienvenida en tu correo</p>
 
-        <p>Te dejamos un regalito de bienvenida en tu correo</p>
-
-        <i class="fa-sharp fa-solid fa-gift"></i>
-        
-    </div>
-    `
-    tarjeta.style.display = "flex"
-}
-}
-
-socio.addEventListener("submit",(event) => {
-    
-    event.preventDefault()
-
-    usuarios.push({
-        nombre: socio.nombre.value,
-        apellido: socio.apellido.value,
-        email: socio.email.value,
-        contrasena: socio.contrasena.value
-
-    })
-
-    if (!nombre.value || !apellido.value || !email.value || !contrasena.value) {
-        Swal.fire({
-            title: 'Por favor complete todos los campos obligatorios',
-            confirmButtonText: "Entendido",
-            confirmButtonColor: '#f9a201',
-            padding: "2rem",
-            icon: "warning",
-            background: "#eee",
-        })
-       
-        }
-    else{
-        tarjetaClub()
+            <i class="fa-sharp fa-solid fa-gift"></i>
+            
+        </div>
+        `
+        tarjeta.style.display = "flex"
+    }
     }
 
-})
+    socio.addEventListener("submit",(event) => {
+        
+        event.preventDefault()
 
+        usuarios.push({
+            nombre: socio.nombre.value,
+            apellido: socio.apellido.value,
+            email: socio.email.value,
+            contrasena: socio.contrasena.value
 
+        })
 
+        if (!nombre.value || !apellido.value || !email.value || !contrasena.value) {
+            Swal.fire({
+                title: 'Por favor complete todos los campos obligatorios',
+                confirmButtonText: "Entendido",
+                confirmButtonColor: '#f9a201',
+                padding: "2rem",
+                icon: "warning",
+                background: "#eee",
+            })
+        
+            }
+        else{
+            tarjetaClub()
+        }
 
+    })
+}
 
 function menuDesplegable(){
     const nav = document.querySelector("#nav")
@@ -85,8 +82,7 @@ function catalogo(){
                 </div>
                 <div class="precio">
                     <a href="#">${producto.precio}</a>
-                </div>
-                
+                </div>    
             </div>
             `
         }   
@@ -94,9 +90,6 @@ function catalogo(){
     .catch(error => console.log(error))
 }
 
-
-
-
-
+formulario();
 catalogo();
 menuDesplegable();
